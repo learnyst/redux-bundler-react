083ab8f573ad084867bd0b278e7930cc754d89dc
keshav Raj <keshavkumark7@gmail.com>
2025-03-20 15:43:53 +0530

    build

--- src/index.js
+++ src/index.js
@@ -1,70 +1,73 @@
-import { createElement, Component, Children } from 'react'
+const React = require('react');
 
-const CONTEXT_TYPES = {
-  store: () => {}
-}
+const StoreContext = React.createContext();
 
-export class Provider extends Component {
-  getChildContext() {
-    return { store: this.props.store }
-  }
-
+class Provider extends React.Component {
   render() {
-    return Children.only(this.props.children)
+    return (
+      <StoreContext.Provider value={this.props.store}>
+        {React.Children.only(this.props.children)}
+      </StoreContext.Provider>
+    );
   }
 }
 
-Provider.childContextTypes = CONTEXT_TYPES
-
-export const connect = (...args) => {
-  const Comp = args.slice(-1)[0]
-  const strings = args.length > 1 ? args.slice(0, -1) : []
-  const actionCreators = []
-  const keysToWatch = []
-  strings.forEach(str => {
-    if (str.slice(0, 6) === 'select') {
-      keysToWatch.push(str)
-      return
+const connect = function (...args) {
+  const Comp = args.slice(-1)[0];
+  const strings = args.length > 1 ? args.slice(0, -1) : [];
+  const actionCreators = [];
+  const keysToWatch = [];
+  
+  strings.forEach((str) => {
+    if (str.startsWith('select')) {
+      keysToWatch.push(str);
+    } else if (str.startsWith('do')) {
+      actionCreators.push(str);
+    } else {
+      throw new Error(`CanNotConnect ${str}`);
     }
-    if (str.slice(0, 2) === 'do') {
-      actionCreators.push(str)
-      return
-    }
-    throw Error(`CanNotConnect ${str}`)
-  })
+  });
 
-  class Connect extends Component {
+  class Connect extends React.Component {
+    static contextType = StoreContext;
+
     constructor(props, context) {
-      super(props, context)
-      const store = context.store
-      this.state = store.select(keysToWatch)
-      this.unsubscribe = store.subscribeToSelectors(
-        keysToWatch,
-        this.setState.bind(this)
-      )
-      this.actionCreators = {}
-      actionCreators.forEach(name => {
+      super(props, context);
+      const store = this.context;
+      this.state = store.select(keysToWatch);
+      this.unsubscribe = store.subscribeToSelectors(keysToWatch, this.setState.bind(this));
+      
+      this.actionCreators = {};
+      actionCreators.forEach((name) => {
         this.actionCreators[name] = (...args) => {
           if (store.action) {
-            return store.action(name, args)
+            return store.action(name, args);
           }
-          return store[name](...args)
-        }
-      })
+          return store[name](...args);
+        };
+      });
     }
 
     componentWillUnmount() {
-      this.unsubscribe()
+      this.unsubscribe();
     }
 
     render() {
-      return createElement(
+      return React.createElement(
         Comp,
-        Object.assign({}, { ...this.props, ref: this.props.refToForward }, this.state, this.actionCreators)
-      )
+        {
+          ...this.props,
+          ...this.state,
+          ...this.actionCreators,
+          ref: this.props.refToForward,
+        }
+      );
     }
   }
-  Connect.contextTypes = CONTEXT_TYPES
-  Connect.displayName = Component.displayName || Component.name
-  return Connect
-}
+
+  Connect.displayName = `Connect(${Comp.displayName || Comp.name})`;
+  return Connect;
+};
+
+exports.Provider = Provider;
+exports.connect = connect;
